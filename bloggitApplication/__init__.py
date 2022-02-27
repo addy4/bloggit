3ca41ca248597db8255993b060b7683ed91b4ecf
aadhaarbhatia.99@gmail.com <PremBhatia1@PREMs-MacBook-Air-3322.local>
2022-02-27 16:32:19 +0530

    secured SMTP mail ID and password

--- bloggitApplication/__init__.py
+++ bloggitApplication/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager 
 from flask_mail import Mail
+import bloggitApplication.keys as keyfile
 
 #SQLAlchemy is an object relational mapper allowing us to access data in an object oriented manner.
 
@@ -13,8 +14,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'GiveEmailIDHere'
-app.config['MAIL_PASSWORD'] = 'GivePasswordHere'
+app.config['MAIL_USERNAME'] = keyfile.KEYS['email']
+app.config['MAIL_PASSWORD'] = keyfile.KEYS['password']
 mail = Mail(app)
 db = SQLAlchemy(app)  
 bcrypt = Bcrypt(app) 
